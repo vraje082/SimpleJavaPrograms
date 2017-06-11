c45ad73b2c755e376243d8fd4b97660b9c12bc9c
Vasanthan Rajendran <vraje082@uottawa.ca>
2017-06-10 22:36:52 -0400

    commit

---
+++ src/hackerrank/Node.java
@@ -0,0 +1,14 @@
+package hackerrank;
+
+/**
+ * Created by Vasanth on 2017-06-10.
+ */
+public class Node {
+    public Node(int data) {
+        this.data = data;
+    }
+    public Node(){}
+
+    int data;
+    Node next;
+}
